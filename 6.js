a7c727c9f3110c9992dfae2fdcfc44c5be3548ee
shakibnymr <shakibnymr11@gmail.com>
2023-08-01 15:57:14 +0600

    pt8.js completed

---
+++ 6.js
@@ -0,0 +1,5 @@
+var phTopics =['html', 'html5', 'css', 'css3', 'tailwind css', 'daisyui', 'responsive website']
+for(var i = 0; i < 7 ; i++){
+    var phTopic = phTopics[i]
+    console.log(phTopic)
+}
---
+++ 7.js
@@ -0,0 +1,11 @@
+var mobilePhonesIUsed = ['nokia', 'walton', 'symphony', 'MI', 'samsung', 'pixel'];
+var index = 0;
+
+
+while ( index < mobilePhonesIUsed.length ){
+    console.log(mobilePhonesIUsed[index]);
+    index++
+}
+
+// console.log(mobilePhonesIUsed[index])
+
