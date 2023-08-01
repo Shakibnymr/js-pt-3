28911ca2f91f412e27ccdbf94a2be1427dc1f5c4
shakibnymr <shakibnymr11@gmail.com>
2023-08-01 23:03:49 +0600

    conditional begins

---
+++ pt10-conditional.js
@@ -0,0 +1,20 @@
+var myMoney = 50000;
+var mac = 80000;
+var laptop = 60000;
+var lenovoYoga = 40000;
+var usedLaptop = 20000;
+if(myMoney >= mac) {
+    console.log('I will buy mac')
+}
+else if (myMoney >= laptop){
+    console.log( 'I will buy laptop' )
+}
+else if (myMoney >= lenovoYoga){
+    console.log( 'I will buy lenovo' )
+}
+else if (myMoney >= usedLaptop){
+    console.log( 'I will buy usedlaptop' )
+}
+else {
+    console.log('I will buy mobile phone')
+}
